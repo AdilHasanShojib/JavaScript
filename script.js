577c14c63e44fb9550ee5bbb06da837fa208f3c2
Md. Shojib Talukder <adilhasanshojib@gmail.com>
2025-05-31 14:40:31 +0600

    DOM Practices part 2

--- script.js
+++ script.js
@@ -4,8 +4,30 @@
 // console.log(tag.innerText);
 
 
-let div=document.querySelectorAll(".box");
-div[0].innerText="Hi";
-div[1].innerText="NextGen";
-div[2].innerText="Innovation Limited!";
-console.log(div);
+// let div=document.querySelectorAll(".box");
+// div[0].innerText="Hi";
+// div[1].innerText="NextGen";
+// div[2].innerText="Innovation Limited!";
+// console.log(div);
+
+          //Append Child
+
+// 
+// const newElement = document.createElement("div");
+// newElement.textContent = "Hello!";
+// parent.appendChild(newElement);
+
+
+       //Remove Child
+
+// const parent = document.querySelector(".container");
+// const child = document.querySelector("#par");
+// parent.removeChild(child);
+
+
+let newbtn=document.createElement("button");
+newbtn.innerText="Click Me!"
+newbtn.style.color="white";
+newbtn.style.backgroundColor="red";
+document.querySelector("body").prepend(newbtn);
+
