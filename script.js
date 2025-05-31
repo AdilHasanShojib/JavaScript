c348b635a78225f1d9cf68f79c37c457f8107f9a
Al Shahriar <shahriarakash@gmailcom>
2025-05-31 12:48:23 +0600

    DOM Practices part 1

---
+++ script.js
@@ -0,0 +1,11 @@
+// let tag=document.querySelector("h1");
+// //console.dir(tag);
+// tag.innerText= tag.innerText+" Innovation Limited!";
+// console.log(tag.innerText);
+
+
+let div=document.querySelectorAll(".box");
+div[0].innerText="Hi";
+div[1].innerText="NextGen";
+div[2].innerText="Innovation Limited!";
+console.log(div);
